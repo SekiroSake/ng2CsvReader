8bd898824cef66dbe46901a88a1392527e3c1bf8
Adam Wang <hwang12wou@gmail.com>
2017-01-19 17:46:15 -0500

    csv reader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  text:any;
+
+  file: File;
+  onChange(event: EventTarget) {
+
+    let eventObj: MSInputMethodContext = <MSInputMethodContext>event;
+    let target: HTMLInputElement = <HTMLInputElement>eventObj.target;
+    let files: FileList = target.files;
+    this.file = files[0];
+    alert(this.file.name);
+
+    let reader = new FileReader();
+    reader.onload = file => {
+      let contents: any = file.target;
+      this.text = contents.result;
+      alert(this.text);
+    }
+    reader.readAsText(this.file);
+
+
+  }
+
+  doAnythingWithFile() {
+  }
 }
+
+// reader = new FileReader();
+// reader.onload = function(e) {
+//   bfile = e.target.result
+//   alert(bfile);   // this shows bfile
+// }
+// reader.readAsText(file);
